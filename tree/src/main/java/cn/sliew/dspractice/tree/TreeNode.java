ca12be79dcdab266ec53c1093792fd73df4ee563
wangqi <1942460489@qq.com>
2020-09-17 21:17:37 +0800

    初始化tree专题模块

---
+++ tree/src/main/java/cn/sliew/dspractice/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package cn.sliew.dspractice.tree;
+
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
