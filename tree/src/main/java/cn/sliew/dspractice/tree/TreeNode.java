36e551ae36b497fb00357a25f93b6952587ffcd4
wangqi <1942460489@qq.com>
2020-09-18 10:58:25 +0800

    初始化题目，设置项目编译jdk版本为8，修改TreeNode工具方法

--- tree/src/main/java/cn/sliew/dspractice/tree/TreeNode.java
+++ tree/src/main/java/cn/sliew/dspractice/tree/TreeNode.java
@@ -1,5 +1,8 @@
 package cn.sliew.dspractice.tree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class TreeNode {
     int value;
     TreeNode left;
@@ -9,39 +12,48 @@ public class TreeNode {
         this.value = value;
     }
 
-    public static TreeNode build(Integer[] array) {
-        int floor = 0, count = 0;
-        TreeNode[] treeNodes = new TreeNode[array.length];
-        while (array != null && count < array.length) {
-            int start = (int) Math.pow(2, floor) - 1;
-            int end = (int) Math.pow(2, floor + 1) - 1;
-            if (end > array.length) {
-                end = array.length;
-            }
-            for (int i = start; i < end; i++) {
-                if (array[i] != null) {
-                    treeNodes[i] = new TreeNode(array[i]);
-                } else {
-                    treeNodes[i] = null;
+    public static TreeNode build(Integer[] nums) {
+        if (nums.length == 0) return new TreeNode(0);
+        Deque<TreeNode> nodeQueue = new LinkedList<>();
+        // 创建一个根节点
+        TreeNode root = new TreeNode(nums[0]);
+        nodeQueue.offer(root);
+        TreeNode cur;
+        // 记录当前行节点的数量（注意不一定是2的幂，而是上一行中非空节点的数量乘2）
+        int lineNodeNum = 2;
+        // 记录当前行中数字在数组中的开始位置
+        int startIndex = 1;
+        // 记录数组中剩余的元素的数量
+        int restLength = nums.length - 1;
+
+        while(restLength > 0) {
+            // 只有最后一行可以不满，其余行必须是满的
+//            // 若输入的数组的数量是错误的，直接跳出程序
+//            if (restLength < lineNodeNum) {
+//                System.out.println("Wrong Input!");
+//                return new TreeNode(0);
+//            }
+            for (int i = startIndex; i < startIndex + lineNodeNum; i = i + 2) {
+                // 说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+                if (i == nums.length) return root;
+                cur = nodeQueue.poll();
+                if (nums[i] != null) {
+                    cur.left = new TreeNode(nums[i]);
+                    nodeQueue.offer(cur.left);
                 }
-                if (i > 0) {
-                    int parent = (i - 1) / 2;
-                    if (parent >= 0) {
-                        TreeNode pNode = treeNodes[parent];
-                        if (pNode != null) {
-                            if (i % 2 == 1) {
-                                pNode.left = treeNodes[i];
-                            } else {
-                                pNode.right = treeNodes[i];
-                            }
-                        }
-                    }
+                // 同上，说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+                if (i + 1 == nums.length) return root;
+                if (nums[i + 1] != null) {
+                    cur.right = new TreeNode(nums[i + 1]);
+                    nodeQueue.offer(cur.right);
                 }
-                count++;
             }
-            floor++;
+            startIndex += lineNodeNum;
+            restLength -= lineNodeNum;
+            lineNodeNum = nodeQueue.size() * 2;
         }
-        return treeNodes[0];
+
+        return root;
     }
 
 }
---
+++ tree/src/main/java/cn/sliew/dspractice/tree/problem/$144BinaryTreePreorderTraversalExample.java
@@ -0,0 +1,24 @@
+package cn.sliew.dspractice.tree.problem;
+
+import cn.sliew.dspractice.tree.TreeNode;
+
+import java.util.List;
+
+/**
+ * 144. Binary Tree Preorder Traversal medium
+ * tags: Stack, Tree
+ */
+public class $144BinaryTreePreorderTraversalExample {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        $144BinaryTreePreorderTraversalExample example = new $144BinaryTreePreorderTraversalExample();
+        Integer[] nums = new Integer[]{1, null, 2, 3};
+        TreeNode root = TreeNode.build(nums);
+        List<Integer> result = example.preorderTraversal(root);
+        System.out.println(result);
+    }
+}
